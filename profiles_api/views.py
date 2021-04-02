3c135779037bc25b74794b3a0c118f65c88a84fb
Heraklaitos <taskin.baltaci@gmail.com>
2021-04-02 22:32:15 +0300

    Added Search functionality

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
+
+from profiles_api import permissions
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -98,3 +103,14 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creaing, creaing and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
